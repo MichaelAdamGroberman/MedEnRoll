cfcaccb42c6dcc285fea02d0d5ac028e9e9ea776
Ying Li <lliying1213@gmail.com>
2021-11-15 20:13:52 -0500

    Update Profile page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <div>Page Not Created Yet,Home</div>;
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,8 @@
+function NotFound() {
+  return (
+    <div>
+      <div>Page Not Created Yet</div>;
+    </div>
+  );
+}
+export default NotFound;
