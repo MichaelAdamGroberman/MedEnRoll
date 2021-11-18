bf1572681d6d1c737fa2235c179eea98ec7a495c
Michael Adam Groberman <michael.groberman@me.com>
2021-11-17 21:46:22 -0500

    committing form files

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import NotFound from './pages/NotFound';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import './App.css';
+import Form from './Form';
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path='/'>
-              <Profile />
-            </Route>
-            <Route exact path='/'>
-              <Profile />
-            </Route>
-            {/* TODO: Insurance information on a separate page? */}
-            {/* <Route exact path='/Profile/:id'>
-              <Vote />
-            </Route> */}
-            <Route>
-              <NotFound />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+  return <Form />;
 }
 
 export default App;
