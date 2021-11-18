bd209c48397293a78b11a088ac1254e1578accf5
Ying Li <lliying1213@gmail.com>
2021-11-18 06:45:33 -0500

    Update Route

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,9 @@
 import Navbar from '../components/Navbar/Navbar';
-import UserForm from '../components/User/UserForm';
 
 function Home() {
   return (
     <div>
-      {/* <Navbar /> */}
-      <UserForm />
+      <Navbar />
     </div>
   );
 }
